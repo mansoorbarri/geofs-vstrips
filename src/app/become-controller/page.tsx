8de22f66e4a1d16727e96ca9b01f18a2b92d1b10
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2025-09-09 00:46:47 +0100

    add auth redirect on /become-controller

--- src/app/become-controller/page.tsx
+++ src/app/become-controller/page.tsx
@@ -1,7 +1,14 @@
 // src/app/become-controller/page.tsx
+"use client";
 import Link from "next/link";
+import { useUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
 
 export default function BecomeControllerPage() {
+  const { user } = useUser();
+  if (user?.publicMetadata.controller == true) { 
+    redirect("/");
+  }
   return (
     <div className="min-h-screen bg-black text-white p-8 flex items-center justify-center">
       <div className="max-w-xl mx-auto py-12 text-center">
