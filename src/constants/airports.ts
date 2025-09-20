a4e827b6191c0e7561fc84d7b077ae5cf957a55a
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2025-09-20 16:52:15 +0100

    fix mispell

--- src/constants/airports.ts
+++ src/constants/airports.ts
@@ -1,7 +1,7 @@
 export const airports = [
   { id: "PHLI", name: "Lihue" }, 
   { id: "PHJR", name: "Kalaeloa" },
-  { id: "PHNL", name: "Hanolulu" },
+  { id: "PHNL", name: "Honolulu" },
   { id: "PHOG", name: "Kahului" },
   { id: "PHKO", name: "Kohala" },
   { id: "PHTO", name: "Hilo"}
