d2160e58b6669184cec88b92720f65c8abcc207f
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2025-09-21 00:01:08 +0100

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,13 @@ export default function Footer() {
       <div className="w-full px-8 py-6">
         <div className="flex flex-col items-center justify-center space-y-2 text-center border-t border-gray-800 pt-6">
           <p className="text-sm text-gray-400">
-            Designed and developed by{" "}
+            developed by{" "}
             <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-200 to-blue-500 hover:from-purple-500 hover:to-blue-400 transition-all duration-300">
-              xyzmani
+              {`xyzmani `}
+            </span>
+            {`& designed by`}
+            <span className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-200 to-blue-500 hover:from-purple-500 hover:to-blue-400 transition-all duration-300">
+              {` luvhummers`}
             </span>
           </p>
           <p className="text-xs text-gray-600">
