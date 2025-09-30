be1759936a7e2d92f91114919dd41ffc274838f6
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2025-09-30 01:12:00 +0100

    update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,25 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export default clerkMiddleware((auth, request: NextRequest) => {
-  // Your custom redirect logic
-  const redirectEnabled = process.env.REDIRECT_HOME_TO_NO_EVENT === "true";
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+    const redirectEnabled = process.env.REDIRECT_HOME_TO_NO_EVENT === "true";
+    if ((req.nextUrl.pathname === "/no-event" ||
+         req.nextUrl.pathname === "/file-flight" || 
+         req.nextUrl.pathname.startsWith("/board/")) && redirectEnabled) {
+      return NextResponse.redirect(new URL("/no-event", req.url));
+    }
   
-  if ((request.nextUrl.pathname === "/" || 
-       request.nextUrl.pathname === "/file-flight" || 
-       request.nextUrl.pathname.startsWith("/board/")) && redirectEnabled) {
-    return NextResponse.redirect(new URL("/no-event", request.url));
-  }
+    if (!isPublicRoute(req)) {
+      await auth.protect()
+    }
   
   // Let Clerk handle the rest
   return NextResponse.next();
-});
+})
+
 
 export const config = {
   matcher: [
